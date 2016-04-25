b9d428bb02a2b0dd9472b71b5d62ad5ee1d642b2
rikari0813 <christiane.maico@gmail.com>
2016-04-25 14:20:36 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,25 @@
-# code your solution here
+def factorial(limit)
+  total = 1
+  i = limit
+  while i > 1
+    total = total * i
+    i = i - 1
+  end
+  total
+end
+
+def sum_of_digits(limit)
+  arrayP = limit.to_s.chars.map(&:to_i)
+  sumT = 0
+  arrayP.each {|n|
+    sumT = sumT + n
+  }
+  sumT
+end
+
+def factorial_digit_sum(limit)
+  newLimit = factorial(limit)
+  newSum = sum_of_digits(newLimit)
+  newSum
+end
+

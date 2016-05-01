eff24e7d14766a69bfe1e87609b1a578512f7f0d
thedailyephemera <thedailyephemera@gmail.com>
2016-04-30 20:00:43 -0400

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,17 @@
 # code your solution here
+def factorial(num)
+	countdown = num - 1
+	until countdown == 1
+		num *= countdown
+		countdown -= 1
+	end
+	num
+end
+
+def sum_of_digits(num)
+	num.to_s.split("").map(&:to_i).inject(0){|sum,x| sum + x}
+end
+
+def factorial_digit_sum(num)
+	factorial(num).to_s.split("").map(&:to_i).inject(0){|sum,x| sum + x}
+end

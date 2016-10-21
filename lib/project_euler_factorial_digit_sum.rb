e5002165ef04bc726607a15be9124880a4084612
Sean Koo <koo.sean@gmail.com>
2016-10-21 03:14:48 +0000

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,21 @@
 # code your solution here
+def factorial(n)
+  answer = 1
+  for i in 1..n
+    answer *=i
+  end
+  return answer
+end
+
+def sum_of_digits(n)
+  stg = n.to_s
+  answer = 0
+  for i in 0..stg.length
+    answer +=stg[i].to_i
+  end
+  return answer
+end
+
+def factorial_digit_sum(n)
+  sum_of_digits(factorial(n))
+end

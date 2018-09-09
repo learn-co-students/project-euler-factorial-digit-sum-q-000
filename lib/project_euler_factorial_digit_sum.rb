98dc59745d26fbe6b2101d39934e347059f8d007
SiamKing <thesiameseelectric@gmail.com>
2018-09-09 14:47:33 -0700

    done

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,11 @@
-# code your solution here
+def factorial(x)
+  x == 1 ? 1 : factorial(x -1) * x
+end
+
+def sum_of_digits(x)
+  x.to_s.split('').inject(0) { |sum, n| sum + n.to_i }
+end
+
+def factorial_digit_sum(x)
+  sum_of_digits(factorial(x))
+end
